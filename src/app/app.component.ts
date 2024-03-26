803d4abe148476f00b5135cd73ccd368d464502d
gabriel.juarez <gabriel.juarez@negocios-digitales.com.mx>
2024-03-26 13:49:02 -0600

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NumericLiteral } from 'typescript';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+ public title: string = 'Mi primera chamba de Angular';
+
+
 }
+
+
